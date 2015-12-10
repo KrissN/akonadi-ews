84225f1bf6c464c8359f0a5ff8dd59f680045dd6
Krzysztof Nowicki <krissn@op.pl>
2015-12-10 12:10:11 +0100

    Restructure to use the new EWS resuest completion signal

--- test/ewstest.cpp
+++ test/ewstest.cpp
@@ -1,22 +1,56 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
 
 #include "ewsclient/ewsclient.h"
 #include "ewsclient/ewsgetfolderrequest.h"
+
+class Test : public QObject
+{
+    Q_OBJECT
+public:
+    Test(QString url, QObject *parent);
+
+public slots:
+    void requestFinished(EwsRequest *req);
+
+private:
+    EwsClient cli;
+    EwsGetFolderRequest *folderReq;
+};
+
+Test::Test(QString url, QObject *parent)
+    : QObject(parent), cli(url)
+{
+    folderReq = new EwsGetFolderRequest(&cli);
+    folderReq->setDistinguishedFolderId(EwsDistinguishedFolderIdItem::Inbox);
+    folderReq->setFolderShape(EwsBaseShapeItem::Default);
+    connect(folderReq, SIGNAL(finished(EwsRequest*)), SLOT(requestFinished(EwsRequest*)));
+    folderReq->send();
+}
+
+void Test::requestFinished(EwsRequest *req)
+{
+    qDebug() << "Request done" << req->isError();
+    if (req->isError()) {
+        qDebug() << req->errorString();
+    }
+    else {
+        EwsGetFolderRequest *folderReq = qobject_cast<EwsGetFolderRequest*>(req);
+        if (!folderReq)
+            return;
+
+    }
+}
 
 int main(int argc, char ** argv)
 {
     QCoreApplication app(argc, argv);
 
-
-    EwsClient cli(argv[1]);
-
-    //cli.getServiceConfiguration(QVector<EwsClient::RequestedConfiguration>() << EwsClient::UnifiedMessagingConfiguration);
-    EwsGetFolderRequest *getFolderReq = new EwsGetFolderRequest(&cli);
-    getFolderReq->setDistinguishedFolderId(EwsDistinguishedFolderIdItem::Inbox);
-    getFolderReq->setFolderShape(EwsBaseShapeItem::Default);
-    getFolderReq->send();
+    Test test(argv[1], &app);
 
     app.exec();
 
     return 0;
 }
+
+#include "ewstest.moc"
