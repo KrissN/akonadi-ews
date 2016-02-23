1848cabe28467b2e34d1272e20c53b414af7165c
Krzysztof Nowicki <krissn@op.pl>
2016-02-23 19:55:03 +0100

    Add MoveFolder request implementation

---
+++ ewsclient/ewsmovefolderrequest.cpp
@@ -0,0 +1,122 @@
+/*  This file is part of Akonadi EWS Resource
+    Copyright (C) 2015-2016 Krzysztof Nowicki <krissn@op.pl>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "ewsmovefolderrequest.h"
+#include "ewsclient_debug.h"
+
+EwsMoveFolderRequest::EwsMoveFolderRequest(EwsClient &client, QObject *parent)
+    : EwsRequest(client, parent)
+{
+}
+
+EwsMoveFolderRequest::~EwsMoveFolderRequest()
+{
+}
+
+void EwsMoveFolderRequest::start()
+{
+    QString reqString;
+    QXmlStreamWriter writer(&reqString);
+
+    startSoapDocument(writer);
+
+    writer.writeStartElement(ewsMsgNsUri, QStringLiteral("MoveFolder"));
+
+    writer.writeStartElement(ewsMsgNsUri, QStringLiteral("ToFolderId"));
+    mDestFolderId.writeFolderIds(writer);
+    writer.writeEndElement();
+
+    writer.writeStartElement(ewsMsgNsUri, QStringLiteral("FolderIds"));
+    Q_FOREACH(const EwsId &id, mIds) {
+        id.writeFolderIds(writer);
+    }
+    writer.writeEndElement();
+
+    writer.writeEndElement();
+
+    endSoapDocument(writer);
+
+    qCDebugNC(EWSRES_REQUEST_LOG) << QStringLiteral("Starting MoveFolder request (%1 folders, to %2)")
+                    .arg(mIds.size()).arg(mDestFolderId.id());
+
+    qCDebug(EWSRES_PROTO_LOG) << reqString;
+
+    prepare(reqString);
+
+    doSend();
+}
+
+bool EwsMoveFolderRequest::parseResult(QXmlStreamReader &reader)
+{
+    return parseResponseMessage(reader, QStringLiteral("MoveFolder"),
+                                [this](QXmlStreamReader &reader) {return parseItemsResponse(reader);});
+}
+
+bool EwsMoveFolderRequest::parseItemsResponse(QXmlStreamReader &reader)
+{
+    Response resp(reader);
+    if (resp.responseClass() == EwsResponseUnknown) {
+        return false;
+    }
+
+    if (EWSRES_REQUEST_LOG().isDebugEnabled()) {
+        if (resp.isSuccess()) {
+            qCDebug(EWSRES_REQUEST_LOG) << QStringLiteral("Got MoveFolder response - OK");
+        }
+        else {
+            qCDebug(EWSRES_REQUEST_LOG) << QStringLiteral("Got MoveFolder response - %1")
+                            .arg(resp.responseMessage());
+        }
+    }
+    mResponses.append(resp);
+    return true;
+}
+
+EwsMoveFolderRequest::Response::Response(QXmlStreamReader &reader)
+    : EwsRequest::Response(reader)
+{
+    if (mClass == EwsResponseParseError) {
+        return;
+    }
+
+    while (reader.readNextStartElement()) {
+        if (reader.namespaceUri() != ewsMsgNsUri && reader.namespaceUri() != ewsTypeNsUri) {
+            setErrorMsg(QStringLiteral("Unexpected namespace in %1 element: %2")
+                .arg(QStringLiteral("ResponseMessage")).arg(reader.namespaceUri().toString()));
+            return;
+        }
+
+        if (reader.name() == QStringLiteral("Folders")) {
+            if (reader.readNextStartElement()) {
+                EwsFolder folder(reader);
+                if (!folder.isValid()) {
+                    return;
+                }
+                mId = folder[EwsFolderFieldFolderId].value<EwsId>();
+
+                // Finish the Folders element.
+                reader.skipCurrentElement();
+            }
+        }
+        else if (!readResponseElement(reader)) {
+            setErrorMsg(QStringLiteral("Failed to read EWS request - invalid response element."));
+            return;
+        }
+    }
+}
---
+++ ewsclient/ewsmovefolderrequest.h
@@ -0,0 +1,67 @@
+/*  This file is part of Akonadi EWS Resource
+    Copyright (C) 2015-2016 Krzysztof Nowicki <krissn@op.pl>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef EWSMOVEFOLDERREQUEST_H
+#define EWSMOVEFOLDERREQUEST_H
+
+#include <QtCore/QList>
+#include <QtCore/QSharedPointer>
+
+#include "ewsfolder.h"
+#include "ewsrequest.h"
+#include "ewstypes.h"
+
+class QXmlStreamReader;
+class QXmlStreamWriter;
+
+class EwsMoveFolderRequest : public EwsRequest
+{
+    Q_OBJECT
+public:
+    class Response : public EwsRequest::Response
+    {
+    public:
+        const EwsId &folderId() const { return mId; };
+    protected:
+        Response(QXmlStreamReader &reader);
+
+        EwsId mId;
+
+        friend class EwsMoveFolderRequest;
+    };
+
+    EwsMoveFolderRequest(EwsClient &client, QObject *parent);
+    virtual ~EwsMoveFolderRequest();
+
+    void setFolderIds(const EwsId::List &ids) { mIds = ids; };
+    void setDestinationFolderId(const EwsId &id) { mDestFolderId = id; };
+
+    virtual void start();
+
+    const QList<Response> &responses() const { return mResponses; };
+protected:
+    virtual bool parseResult(QXmlStreamReader &reader);
+    bool parseItemsResponse(QXmlStreamReader &reader);
+private:
+    EwsId::List mIds;
+    EwsId mDestFolderId;
+    QList<Response> mResponses;
+};
+
+#endif
