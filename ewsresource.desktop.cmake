9389a8b66ef713060e9bc3c8edaf9e6c6b64ed46
Krzysztof Nowicki <krissn@op.pl>
2016-02-25 20:22:07 +0100

    Add icons

--- ewsresource.desktop.cmake
+++ ewsresource.desktop.cmake
@@ -3,6 +3,7 @@ Name=Microsoft Exchange Server (EWS)
 Comment="Provides access to mail, appointments tasks and contacts of a Microsoft Exchange server using EWS"
 Type=AkonadiResource
 Exec=akonadi_ews_resource
+Icon=akonadi-ews
 
 X-Akonadi-MimeTypes=text/directory,message/rfc822;text/calendar,application/x-vnd.kde.contactgroup,application/x-vnd.akonadi.calendar.event,application/x-vnd.akonadi.calendar.todo
 X-Akondai-Capabilities=Resource,NeedsNetwork,@EWS_MTA_CAPABILITIES@
