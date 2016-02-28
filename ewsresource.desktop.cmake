3bfbe3d4ef3e2a97d876c9fbbf6e1e494376e7bd
Krzysztof Nowicki <krissn@op.pl>
2016-02-28 11:51:43 +0100

    Fix nasty typo in desktop files

--- ewsresource.desktop.cmake
+++ ewsresource.desktop.cmake
@@ -5,6 +5,6 @@ Type=AkonadiResource
 Exec=akonadi_ews_resource
 Icon=akonadi-ews
 
-X-Akonadi-MimeTypes=text/directory,message/rfc822;text/calendar,application/x-vnd.kde.contactgroup,application/x-vnd.akonadi.calendar.event,application/x-vnd.akonadi.calendar.todo
-X-Akondai-Capabilities=Resource,NeedsNetwork,@EWS_MTA_CAPABILITIES@
+X-Akonadi-MimeTypes=text/directory,message/rfc822,text/calendar,application/x-vnd.kde.contactgroup,application/x-vnd.akonadi.calendar.event,application/x-vnd.akonadi.calendar.todo
+X-Akonadi-Capabilities=Resource,NeedsNetwork,@EWS_MTA_CAPABILITIES@
 X-Akonadi-Identifier=akonadi_ews_resource
