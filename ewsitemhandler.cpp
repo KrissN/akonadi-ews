60ae431955c7069de77233b7bb09ceb52634c4e4
Krzysztof Nowicki <krissn@op.pl>
2016-04-28 22:01:24 +0200

    Fix silly flags bug that prevented them from being read at all

--- ewsitemhandler.cpp
+++ ewsitemhandler.cpp
@@ -105,7 +105,7 @@ QSet<QByteArray> EwsItemHandler::readFlags(const EwsItem &item)
     QVariant flagProp = item[EwsResource::flagsProperty];
     if (!flagProp.isNull() && (flagProp.canConvert<QStringList>())) {
         QStringList flagList = flagProp.toStringList();
-        Q_FOREACH(const QString &flag, flags) {
+        Q_FOREACH(const QString &flag, flagList) {
             flags.insert(flag.toAscii());
         }
     }
